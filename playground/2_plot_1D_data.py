c805e01da7d807d03856cca54b41a90de35dbc7a
Bruce Coburn <brucebcoburn@gmail.com>
2023-08-03 16:25:07 -0600

    Adding some comments

--- playground/2_plot_1D_data.py
+++ playground/2_plot_1D_data.py
@@ -3,19 +3,16 @@ import sys
 from PyQt5 import QtWidgets
 
 # Import custom modules
-from pozyx_helpers.PozyxPlot import plot_csv_data
 from pozyx_helpers.PozyxPlot import QtPlotWindow
 
 
-
 if __name__ == "__main__":
-    """
-    csv_file_path = (
-        "pozyx_ranging_runs/data_2023-08-03_12-33-23_PRECISION.csv"
-    )
-    plot_csv_data(csv_file_path)
-    """
+    # Create a Qt application
     app = QtWidgets.QApplication(sys.argv)
-    window = QtPlotWindow()
+    window = QtPlotWindow()  # QtPlotWindow() will plot the data from a .csv file
+
+    # Show the Qt application
     window.show()
+
+    # On exit, close the Qt application
     sys.exit(app.exec_())
--- playground/pozyx_helpers/PozyxPlot.py
+++ playground/pozyx_helpers/PozyxPlot.py
@@ -8,23 +8,30 @@ from PyQt5 import QtWidgets
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from PyQt5.QtWidgets import QFileDialog
 
+
 def plot_csv_data(csv_file):
+    """
+    Plots the data from a .csv file using a Matplotlib popup window.
+    """
+
     x_data = []
     y_data = []
 
+    # Read data from csv file
     with open(csv_file, "r") as file:
         reader = csv.reader(file)
         headers = next(reader)  # Skip the header row
 
         for row in reader:
             try:
-                x_value = float(row[0])  # Second column (x-axis)
-                y_value = float(row[1])  # First column (y-axis)
+                x_value = float(row[0])  # First column (x-axis)
+                y_value = float(row[1])  # Second column (y-axis)
                 x_data.append(x_value)
                 y_data.append(y_value)
             except ValueError:
                 print(f"Invalid row data: {row}")
 
+    # Plot data
     plt.plot(x_data, y_data, marker="o")
     plt.xlabel(headers[0])  # x-axis label from the header
     plt.ylabel(headers[1])  # y-axis label from the header
@@ -32,38 +39,61 @@ def plot_csv_data(csv_file):
     plt.grid(True)
     plt.show()
 
+
 class QtPlotWindow(QtWidgets.QWidget):
+    """
+    A Qt window that plots data from a .csv file.
+    """
     def __init__(self):
         super().__init__()
 
-        self.initUI()
+        self.initUI() # Initialize the UI
 
     def initUI(self):
+        """
+        Initializes the UI of the Qt window.
+        """
 
-        self.setWindowTitle('Pozyx 1-D Plotter')
+        # Set window title
+        self.setWindowTitle("Pozyx 1-D Plotter")
 
+        # Create a figure and canvas
         self.figure, self.ax = plt.subplots()
         self.canvas = FigureCanvas(self.figure)
 
-        self.button = QtWidgets.QPushButton('Select CSV File')
-        self.button.clicked.connect(self.loadCsvFile)
+        # Create a button to select a .csv file
+        self.button = QtWidgets.QPushButton("Select CSV File")
+        self.button.clicked.connect(self.loadCsvFile) # Connect the button to a function
 
+        # Create a vertical box layout
         layout = QtWidgets.QVBoxLayout()
         layout.addWidget(self.button)
         layout.addWidget(self.canvas)
 
+        # Set the layout of the window
         self.setLayout(layout)
 
     def loadCsvFile(self):
+        """
+        Opens a file dialog to select a .csv file.
+        """
         options = QFileDialog.Options()
-        filename, _ = QFileDialog.getOpenFileName(self, 'Select CSV File', '', 'CSV Files (*.csv)', options=options)
 
+        # Open a file dialog to select a .csv file, filters for .csv files
+        filename, _ = QFileDialog.getOpenFileName(
+            self, "Select CSV File", "", "CSV Files (*.csv)", options=options
+        )
+
+        # Plot the .csv file
         if filename:
             self.plotCsvFile(filename)
 
     def plotCsvFile(self, filename):
+        """
+        Plots the data from a .csv file.
+        """
         df = pd.read_csv(filename)
         self.ax.clear()
         self.ax.plot(df[df.columns[0]], df[df.columns[1]])
-        self.ax.set_title('1-D Pozyx Data')
+        self.ax.set_title("1-D Pozyx Data")
         self.canvas.draw()
